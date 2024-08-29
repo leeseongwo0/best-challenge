a622f3633dccc9c6633e9a079e7b227f0093da0f
Seongwoo Lee <tjdrn0516@gmail.com>
2024-08-29 01:23:43 +0000

    add IndieProjectFunding.sol

---
+++ contract/contracts/IndieProjectFunding.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract IndieProjectFunding {
+    struct IndieProject {
+        uint totalGT;
+        ProjectDAO dao;
+    }
+
+    struct ProjectDAO {
+        address teamAccount;
+        GovernanceVault vault;  
+        mapping(address => Supporter) supporters;
+        address creator;
+        uint proposalCount;
+        mapping(uint => Proposal) proposals;
+    }
+
+    struct GovernanceVault {  
+        uint ethBalance;
+        uint GTBalance;
+    }
+
+    struct Supporter {
+        uint GTBalance;
+        uint ethBalance;
+    }
+
+    struct Proposal {
+        string title;
+        uint amount;
+        uint yesVotes;
+        uint noVotes;
+        uint endTime;
+        bool executed;
+        mapping(address => bool) voters;
+    }
+
+    IndieProject[] public projects;
+
+    function createProject(address _teamAccount, address _creator) public {
+        ProjectDAO memory newDAO = ProjectDAO({
+            teamAccount: _teamAccount,
+            vault: GovernanceVault({  
+                ethBalance: 0,
+                GTBalance: 0
+            }),
+            creator: _creator,
+            proposalCount: 0
+        });
+
+        IndieProject memory newProject = IndieProject({
+            totalGT: 0,
+            dao: newDAO
+        });
+
+        projects.push(newProject);
+    }
+
+    function startFunding(uint projectIndex, uint _requiredEth) public {
+        IndieProject storage project = projects[projectIndex];
+        project.totalGT = _requiredEth * 10;
+        project.dao.vault.GTBalance = project.totalGT;  
+    }
+
+    function contribute(uint projectIndex) public payable {
+        IndieProject storage project = projects[projectIndex];
+        require(msg.value > 0, "You must send some Ether");
+
+        project.dao.supporters[msg.sender].ethBalance += msg.value;
+        uint GTAmount = msg.value;
+        project.dao.supporters[msg.sender].GTBalance += GTAmount;
+
+        project.dao.vault.ethBalance += msg.value;  
+        }
+
+    function propose(uint projectIndex, string memory proposalTitle, uint _amount) public {
+        IndieProject storage project = projects[projectIndex];
+        //require(msg.sender == project.dao.creator, "Only the creator can propose");
+
+        Proposal storage newProposal = project.dao.proposals[project.dao.proposalCount];
+        newProposal.title = proposalTitle;
+        newProposal.amount = _amount;
+        newProposal.endTime = block.timestamp + 1 weeks;
+        newProposal.executed = false;
+        
+        project.dao.proposalCount++;
+    }
+
+    function vote(uint projectIndex, uint proposalIndex, bool approve) public {
+        IndieProject storage project = projects[projectIndex];
+        Proposal storage proposal = project.dao.proposals[proposalIndex];
+        
+        require(block.timestamp < proposal.endTime, "Voting period has ended");
+        //require(project.dao.supporters[msg.sender].GTBalance > 0, "You are not a supporter");
+        require(!proposal.voters[msg.sender], "You have already voted");
+        
+        if (approve) {
+            proposal.yesVotes += project.dao.supporters[msg.sender].GTBalance;
+        } else {
+            proposal.noVotes += project.dao.supporters[msg.sender].GTBalance;
+        }
+        
+        proposal.voters[msg.sender] = true;
+    }
+
+    function executeProposal(uint projectIndex, uint proposalIndex) public {
+        IndieProject storage project = projects[projectIndex];
+        Proposal storage proposal = project.dao.proposals[proposalIndex];
+        
+        require(block.timestamp >= proposal.endTime, "Voting period has not ended");
+        require(!proposal.executed, "Proposal has already been executed");
+        
+        if (proposal.yesVotes > proposal.noVotes) {
+            require(proposal.amount <= project.dao.vault.ethBalance, "Not enough funds in governance vault"); 
+            payable(project.dao.teamAccount).transfer(proposal.amount);
+            project.dao.vault.ethBalance -= proposal.amount;  
+        }
+        
+        proposal.executed = true;
+    }
+}
